b965dab1a2945083a8e45e3440226d6d60e279c5
fedorchaklm <fedorchak.luba@gmail.com>
2025-02-01 16:43:24 +0200

    in progress

--- src/layouts/main-layout/MainLayout.tsx
+++ src/layouts/main-layout/MainLayout.tsx
@@ -1,18 +1,27 @@
-import {Outlet} from "react-router";
+import {Link, Outlet} from "react-router";
 import {Menu} from "../../components/menu/Menu.tsx";
 import {Logo} from "../../components/logo/Logo.tsx";
 import {useAppSelector} from "../../redux/hooks/useAppSelector.ts";
 import './MainLayout.css';
-import {ProtectedRoute} from "../../components/protected-router/ProtectedRouter.tsx";
 
 export const MainLayout = () => {
     const {currentUser} = useAppSelector(({currentUserSlice}) => currentUserSlice);
 
-    return (currentUser &&
-        <ProtectedRoute>
-            <Menu/>
-            <Logo img={currentUser.image} alt={currentUser.lastName}/>
+    if (!currentUser) {
+        return (
+            <div className='bg-white text-black text-3xl w-fit px-4 py-4 rounded-xl'>
+                <p>You need to authenticate</p> <Link className='text-gray-500 hover:underline' to='/login'>Login</Link>
+            </div>
+        );
+    }
+
+    return (
+        <>
+            <div className='flex justify-between px-4 bg-black text-white w-full'>
+                <Menu/>
+                <Logo img={currentUser.image} alt={currentUser.lastName}/>
+            </div>
             <Outlet/>
-        </ProtectedRoute>
+        </>
     );
 }
